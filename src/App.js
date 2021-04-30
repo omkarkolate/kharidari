7321016de7c493195bde5d00ee581be1643c6a4a
Omkar Kolate <53403067+omkarkolate@users.noreply.github.com>
2021-04-30 20:22:40 +0530

    feat: Homepage Added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./styles.css";
+import Homepage from "./pages/Homepage";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Kharidari</h1>
+      {/* <h1>Kharidari</h1> */}
+      <Homepage />
     </div>
   );
 }
