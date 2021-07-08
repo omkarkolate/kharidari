a0c019bc183730bc6296cd9cd0ef3aafdb969d35
Omkar Kolate <53403067+omkarkolate@users.noreply.github.com>
2021-07-08 20:56:56 +0530

    feat: Added sort and filter slider menu

--- src/components/sortAndFilter/SortAndFilter.jsx
+++ src/components/sortAndFilter/SortAndFilter.jsx
@@ -1,10 +1,78 @@
+import { useState } from "react";
 import styles from "./sortAndFilter.module.css";
 
 export function SortAndFilter() {
+  const [isShowMenu, setIsShowMenu] = useState(false);
+  const [menu, setMenu] = useState(null);
+
+  const sortMenu = (
+    <fieldset className={styles["sort-by"]}>
+      <legend>Sort By</legend>
+      <div className={styles["field"]}>
+        <label htmlFor="PRICE_LOW_TO_HIGH">Price: Low to High</label>
+        <input
+          type="radio"
+          id="PRICE_LOW_TO_HIGH"
+          name="SORT"
+          value="PRICE_LOW_TO_HIGH"
+        />
+      </div>
+      <div className={styles["field"]}>
+        <label htmlFor="PRICE_HIGH_TO_LOW">Price: High to Low</label>
+        <input
+          type="radio"
+          id="PRICE_HIGH_TO_LOW"
+          name="SORT"
+          value="PRICE_HIGH_TO_LOW"
+        />
+      </div>
+    </fieldset>
+  );
+
+  const filterMenu = (
+    <fieldset className={styles["filter-by"]}>
+      <legend>Filter By</legend>
+      <div className={styles["field"]}>
+        <label htmlFor="INCLUDE_OUT_OF_STOCK">Include out of stock</label>
+        <input
+          type="checkbox"
+          id="INCLUDE_OUT_OF_STOCK"
+          name="INCLUDE_OUT_OF_STOCK"
+          value="INCLUDE_OUT_OF_STOCK"
+        />
+      </div>
+      <div className={styles["field"]}>
+        <label htmlFor="FREE_DELIVERY_ONLY">Free delivery only</label>
+        <input
+          type="checkbox"
+          id="FREE_DELIVERY_ONLY"
+          name="FREE_DELIVERY_ONLY"
+          value="FREE_DELIVERY_ONLY"
+        />
+      </div>
+    </fieldset>
+  );
+
+  const menuWrapper = (
+    <div className={styles["menu-wrapper"]}>
+      <div className={styles["cover"]} onClick={selectMenu}></div>
+      <div className={styles["menu"]}>{menu}</div>
+    </div>
+  );
+
+  function selectMenu(selectedMenu) {
+    if (!isShowMenu) {
+      setIsShowMenu(true);
+      setMenu(selectedMenu);
+    } else {
+      setIsShowMenu(false);
+    }
+  }
+
   return (
-    <div className={styles["wrapper"]}>
+    <div>
       <div className={styles["mobile-view"]}>
-        <div className={styles["sort"]}>
+        <div className={styles["sort"]} onClick={() => selectMenu(sortMenu)}>
           <div className={styles["icon"]}>
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -25,7 +93,10 @@ export function SortAndFilter() {
           </div>
           Sort
         </div>
-        <div className={styles["filter"]}>
+        <div
+          className={styles["filter"]}
+          onClick={() => selectMenu(filterMenu)}
+        >
           <div className={styles["icon"]}>
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -48,49 +119,9 @@ export function SortAndFilter() {
       </div>
 
       <div className={styles["desktop-view"]}>
-        <fieldset className={styles["sort-by"]}>
-          <legend>Sort By</legend>
-          <div className={styles["field"]}>
-            <label htmlFor="PRICE_LOW_TO_HIGH">Price: Low to High</label>
-            <input
-              type="radio"
-              id="PRICE_LOW_TO_HIGH"
-              name="SORT"
-              value="PRICE_LOW_TO_HIGH"
-            />
-          </div>
-          <div className={styles["field"]}>
-            <label htmlFor="PRICE_HIGH_TO_LOW">Price: High to Low</label>
-            <input
-              type="radio"
-              id="PRICE_HIGH_TO_LOW"
-              name="SORT"
-              value="PRICE_HIGH_TO_LOW"
-            />
-          </div>
-        </fieldset>
-        <fieldset className={styles["filter-by"]}>
-          <legend>Filter By</legend>
-          <div className={styles["field"]}>
-            <label htmlFor="INCLUDE_OUT_OF_STOCK">Include out of stock</label>
-            <input
-              type="checkbox"
-              id="INCLUDE_OUT_OF_STOCK"
-              name="INCLUDE_OUT_OF_STOCK"
-              value="INCLUDE_OUT_OF_STOCK"
-            />
-          </div>
-          <div className={styles["field"]}>
-            <label htmlFor="FREE_DELIVERY_ONLY">Free delivery only</label>
-            <input
-              type="checkbox"
-              id="FREE_DELIVERY_ONLY"
-              name="FREE_DELIVERY_ONLY"
-              value="FREE_DELIVERY_ONLY"
-            />
-          </div>
-        </fieldset>
+        {sortMenu} {filterMenu}
       </div>
+      {isShowMenu && menuWrapper}
     </div>
   );
 }
