68cbe5a0b48680fb4f822eaf934cf666e161a0a9
Omkar Kolate <53403067+omkarkolate@users.noreply.github.com>
2021-07-17 11:58:07 +0530

    style: added media queries

--- src/pages/address/Address.jsx
+++ src/pages/address/Address.jsx
@@ -4,37 +4,39 @@ import { Link } from "react-router-dom";
 
 export function Address() {
   return (
-    <div className={styles["address-page"]}>
+    <div>
       <Header title="My Addresses" />
-      <Link to="/address/add-new-address">
-        <div className={styles["add-new"]}>+ Add a new address</div>
-      </Link>
-      <div className={styles["address-list"]}>
-        <div className={styles["address-card"]}>
-          <div className={styles["address-select-btn"]}>
-            <input type="radio" name="SELECTED-ADDRESS" />
-          </div>
-          <div>
-            <div className={styles["address-name"]}>Omkar Kolate</div>
-            <div className={styles["address-details"]}>
-              Mauli, Vanipeth, A/P Amba, Tal - Shahuwadi, Kolhapur District,
-              Maharashtra - 415101
+      <div className={styles["address-page"]}>
+        <Link to="/address/add-new-address">
+          <div className={styles["add-new"]}>+ Add a new address</div>
+        </Link>
+        <div className={styles["address-list"]}>
+          <div className={styles["address-card"]}>
+            <div className={styles["address-select-btn"]}>
+              <input type="radio" name="SELECTED-ADDRESS" />
             </div>
-            <div className={styles["address-mobileno"]}>8888540008</div>
+            <div>
+              <div className={styles["address-name"]}>Omkar Kolate</div>
+              <div className={styles["address-details"]}>
+                Mauli, Vanipeth, A/P Amba, Tal - Shahuwadi, Kolhapur District,
+                Maharashtra - 415101
+              </div>
+              <div className={styles["address-mobileno"]}>8888540008</div>
+            </div>
           </div>
-        </div>
 
-        <div className={styles["address-card"]}>
-          <div className={styles["address-select-btn"]}>
-            <input type="radio" name="SELECTED-ADDRESS" />
-          </div>
-          <div>
-            <div className={styles["address-name"]}>Omkar Kolate</div>
-            <div className={styles["address-details"]}>
-              Mauli, Vanipeth, A/P Amba, Tal - Shahuwadi, Kolhapur District,
-              Maharashtra - 415101
+          <div className={styles["address-card"]}>
+            <div className={styles["address-select-btn"]}>
+              <input type="radio" name="SELECTED-ADDRESS" />
             </div>
-            <div className={styles["address-mobileno"]}>8888540008</div>
+            <div>
+              <div className={styles["address-name"]}>Omkar Kolate</div>
+              <div className={styles["address-details"]}>
+                Mauli, Vanipeth, A/P Amba, Tal - Shahuwadi, Kolhapur District,
+                Maharashtra - 415101
+              </div>
+              <div className={styles["address-mobileno"]}>8888540008</div>
+            </div>
           </div>
         </div>
       </div>
--- src/pages/checkout/Checkout.jsx
+++ src/pages/checkout/Checkout.jsx
@@ -6,79 +6,82 @@ export function Checkout() {
   return (
     <div className={styles["checkout-page"]}>
       <Header brandName title="Order Details" />
-      <div className={styles["delivery-address"]}>
-        <div className={styles["address-card"]}>
-          <div className={styles["address-name"]}>Omkar Kolate</div>
-          <div className={styles["address-details"]}>
-            Mauli, Vanipeth, A/P Amba, Tal - Shahuwadi, Kolhapur District,
-            Maharashtra - 415101
-          </div>
-          <div className={styles["address-mobileno"]}>8888540008</div>
-        </div>
-        <Link to="/address">
-          <div className={styles["change-address-btn"]}>
-            Change or Add Address
-          </div>
-        </Link>
-      </div>
 
       <div className={styles["cart-flex-wrapper"]}>
-        <div className={styles["cart-product-list"]}>
-          <div className={styles["cart-item-card"]}>
-            <div className={styles["card-flex-wrapper"]}>
-              <div className={styles["card-left"]}>
-                <div className={styles["product-name"]}>PUMA T-shirt</div>
-                <div className={styles["product-price"]}>₹604</div>
+        <div className={styles["checkout-flex-left"]}>
+          <div className={styles["delivery-address"]}>
+            <div className={styles["address-card"]}>
+              <div className={styles["address-name"]}>Omkar Kolate</div>
+              <div className={styles["address-details"]}>
+                Mauli, Vanipeth, A/P Amba, Tal - Shahuwadi, Kolhapur District,
+                Maharashtra - 415101
               </div>
-              <div className={styles["card-right"]}>
-                <div className={styles["product-image"]}>
-                  <img
-                    src={`https://picsum.photos/70/100?random=1`}
-                    alt="product"
-                  />
-                </div>
-                <div className={styles["product-quantity"]}>
-                  <select>
-                    <option value="1">Qty: 1</option>
-                    <option value="2">Qty: 2</option>
-                    <option value="3">Qty: 3</option>
-                    <option value="4">Qty: 4</option>
-                  </select>
-                </div>
-              </div>
+              <div className={styles["address-mobileno"]}>8888540008</div>
             </div>
-          </div>
-
-          <div className={styles["cart-item-card"]}>
-            <div className={styles["card-flex-wrapper"]}>
-              <div className={styles["card-left"]}>
-                <div className={styles["product-name"]}>PUMA T-shirt</div>
-                <div className={styles["product-price"]}>₹604</div>
+            <Link to="/address">
+              <div className={styles["change-address-btn"]}>
+                Change or Add Address
               </div>
-              <div className={styles["card-right"]}>
-                <div className={styles["product-image"]}>
-                  <img
-                    src={`https://picsum.photos/70/100?random=2`}
-                    alt="product"
-                  />
-                </div>
-                <div className={styles["product-quantity"]}>
-                  <select>
-                    <option value="1">Qty: 1</option>
-                    <option value="2">Qty: 2</option>
-                    <option value="3">Qty: 3</option>
-                    <option value="4">Qty: 4</option>
-                  </select>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div className={styles["place-order-bar"]}>
-            <div className={styles["total-amount"]}>24,998</div>
-            <Link to="/successful">
-              <div className={styles["place-order-btn"]}>CONTINUE</div>
             </Link>
+          </div>
+
+          <div className={styles["cart-product-list"]}>
+            <div className={styles["cart-item-card"]}>
+              <div className={styles["card-flex-wrapper"]}>
+                <div className={styles["card-left"]}>
+                  <div className={styles["product-name"]}>PUMA T-shirt</div>
+                  <div className={styles["product-price"]}>₹604</div>
+                </div>
+                <div className={styles["card-right"]}>
+                  <div className={styles["product-image"]}>
+                    <img
+                      src={`https://picsum.photos/70/100?random=1`}
+                      alt="product"
+                    />
+                  </div>
+                  <div className={styles["product-quantity"]}>
+                    <select>
+                      <option value="1">Qty: 1</option>
+                      <option value="2">Qty: 2</option>
+                      <option value="3">Qty: 3</option>
+                      <option value="4">Qty: 4</option>
+                    </select>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className={styles["cart-item-card"]}>
+              <div className={styles["card-flex-wrapper"]}>
+                <div className={styles["card-left"]}>
+                  <div className={styles["product-name"]}>PUMA T-shirt</div>
+                  <div className={styles["product-price"]}>₹604</div>
+                </div>
+                <div className={styles["card-right"]}>
+                  <div className={styles["product-image"]}>
+                    <img
+                      src={`https://picsum.photos/70/100?random=2`}
+                      alt="product"
+                    />
+                  </div>
+                  <div className={styles["product-quantity"]}>
+                    <select>
+                      <option value="1">Qty: 1</option>
+                      <option value="2">Qty: 2</option>
+                      <option value="3">Qty: 3</option>
+                      <option value="4">Qty: 4</option>
+                    </select>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className={styles["place-order-bar"]}>
+              <div className={styles["total-amount"]}>24,998</div>
+              <Link to="/successful">
+                <div className={styles["place-order-btn"]}>CONTINUE</div>
+              </Link>
+            </div>
           </div>
         </div>
 
